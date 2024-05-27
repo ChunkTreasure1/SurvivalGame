91b59ea06e2dfe9118bbc66eb7244b96fc9b149d
Ivar Jönsson <chunktreasure@gmail.com>
2024-05-27 22:33:07 +0200

    Update

--- Assets/Scripts/Source/Player/Player.cs
+++ Assets/Scripts/Source/Player/Player.cs
@@ -5,9 +5,21 @@ namespace SurvivalGame
     class Player : Script
     {
         public Entity CameraEntity;
+        
+        public float MaxStamina = 100f;
+        public float StaminaDecreasePerSecond = 5;
+        public float StaminaIncreasePerSecond = 3;
+        public float MinSprintStaminaAmount = 10;
+
         public float Speed = 100f;
+        public float SprintSpeed = 150f;
+        public float JumpForce = 100f;
+
+        public float MaxItemGrabLength = 200f;
 
         private CharacterControllerComponent m_characterController;
+        private bool m_disableSprinting = false;
+        private float m_currentStaminaAmount = 0f;
 
         private void OnCreate()
         {
@@ -17,6 +29,7 @@ namespace SurvivalGame
             }
 
             m_characterController = entity.GetComponent<CharacterControllerComponent>();
+            m_currentStaminaAmount = MaxStamina;
         }
 
         private void OnUpdate(float deltaTime)
@@ -57,7 +70,54 @@ namespace SurvivalGame
                 resultDir += right;
             }
 
-            m_characterController.Move(resultDir * Speed * deltaTime);
+            if (Input.IsKeyPressed(KeyCode.Space))
+            {
+                m_characterController.Jump(JumpForce);
+            }
+
+            bool isSprinting = Input.IsKeyDown(KeyCode.Left_Shift) && !m_disableSprinting;
+
+            if (Input.IsKeyReleased(KeyCode.Left_Shift))
+            {
+                m_disableSprinting = false;
+            }
+
+            if (isSprinting && m_currentStaminaAmount < MinSprintStaminaAmount)
+            {
+                isSprinting = false;
+                m_disableSprinting = true;
+            }
+
+            if (isSprinting)
+            {
+                m_currentStaminaAmount -= StaminaDecreasePerSecond * deltaTime;
+                if (m_currentStaminaAmount < 0f)
+                {
+                    m_currentStaminaAmount = 0f;
+                }
+            }
+            else
+            {
+                m_currentStaminaAmount += StaminaIncreasePerSecond * deltaTime;
+                if (m_currentStaminaAmount > MaxStamina)
+                {
+                    m_currentStaminaAmount = MaxStamina;
+                }
+            }
+
+            m_characterController.Move(resultDir * (isSprinting ? SprintSpeed : Speed) * deltaTime);
+        }
+
+        private void OnFixedUpdate(float deltaTime)
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(CameraEntity.position, CameraEntity.forward, out hit, MaxItemGrabLength, 2))
+            {
+                if (Input.IsKeyPressed(KeyCode.E))
+                {
+                    Log.Trace("Item picked up!");
+                }
+            }
         }
     }
 }
